fd644e07b03ca52741cc7dc41ab676a982bf68c1
marlonrodri7 <marlon7piri@gmail.com>
2023-12-26 19:35:48 -0500

    add nexthauth 6

--- src/app/client/NavClient.jsx
+++ src/app/client/NavClient.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import HamburguesaIcon from "../components/icons/HamburguesaIcon";
+import { button } from "@nextui-org/react";
 
 const NavClient = () => {
   const { data: session, status } = useSession();
@@ -25,12 +26,14 @@ const NavClient = () => {
     <div className="w-full  p-4 overflow-hidden">
       <div className="w-full flex justify-between items-center overflow-hidden">
         <h2>Pizzas.Hub</h2>
+        <span>{session?.user?.name}</span>
         {status ==='loading' ? 'loading...':""}
 
         <ul className="menu">
           <li>Home</li>
           <li>Productos</li>
           <li>Contacto</li>
+          {!session?.user?.name ? <button onClick={signIn}>Login</button> : <button onClick={signOut}>Logout</button>}
         </ul>
         <div className="menu_hamburguesa" onClick={()=>openMenu()}>
           <HamburguesaIcon className="w-14 h-14"/>
