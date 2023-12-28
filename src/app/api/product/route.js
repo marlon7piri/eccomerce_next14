47dfd821bfb24d0567d9c9544ae93a4c687f8169
marlonrodri7 <marlon7piri@gmail.com>
2023-12-28 02:08:46 -0500

    add new function upload image 4

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -20,39 +20,5 @@ export async function GET() {
  
 }
 
-export async function POST(req) {
-  const data = await req.formData();
-  const title = data.get("title");
-  const price = data.get("price");
-  const description = data.get("description");
-  const stock = data.get("stock");
-  const image = data.get("image");
-  const rating = data.get("rating");
 
-
-
-  const productnew = {
-    title,
-    price,
-    description,
-    stock,
-    rating,
-    image:image || "",
-    publicId
-  }
-  try {
-    await connectDb();
-    const allproducts = await new Products(productnew);
-
-
-   
-    const productsaved = await allproducts.save();
-
-    if (!productsaved) return NextResponse.status(404);
-
-    return NextResponse.json(productsaved);
-  } catch (error) {
-    return NextResponse.json({ message: error });
-  }
-}
 
